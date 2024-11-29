936668924bf9184312a87914bfc847eaee8f37c6
jrteodoro7 <jrteodoro2008@gmail.com>
2024-11-29 10:50:03 -0300

    2911

--- servidor/index.js
+++ servidor/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const cors = require('cors');
+const mysql = requirer('mysql');
+const app = express();
+const PORT = 3001;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+app.use(cors());
+app.use(express.json());
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'aulabd'
+});
+
+db.connect((erro) => {
+    if (erro) {
+        console.error('Erro ao conectar ao MySQL:', erro);
+    } else {
+        console.log('Conectado ao MySQL com sucesso!');
+    }
+});
